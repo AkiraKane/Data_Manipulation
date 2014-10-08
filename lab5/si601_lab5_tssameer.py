114bbbb39318cc4e24fca490d239da398b2a6232
Sameer Tammana <saisameer.t@gmail.com>
2014-10-08 12:34:55 -0400

    Lab 5 completed

--- lab5/si601_lab5_tssameer.py
+++ lab5/si601_lab5_tssameer.py
@@ -5,23 +5,28 @@
 from mrjob.job import MRJob
 import re
 
-WORD_RE = re.compile(r"\b[\w]+\s[\w]+\b")
+WORD_RE = re.compile(r"[\w]+")
+f = open(r'si601_lab5_output_tssameer.txt', 'w')
 
 class BiGramFreqCount(MRJob):
-  
   ### input: self, ignored_key, in_value
   def mapper(self, _, line):
-      for word in WORD_RE.findall(line):
-          yield (word.lower(), 1)
-    ### replace this part and use bigram (this word and its next word) as the key
-    ### skip the last word because there is no word after it
+      words = WORD_RE.findall(line)
+      for i in range(len(words)-1):
+          exp = r"\b"+words[i]+r"[.,-\?/!\|/(/)/'"+'/"'+ "/ ]+"+words[i+1]+r"\b"
+          test = re.compile(exp)
+          res = test.search(line)
+          if res:
+              ans = words[i]+' '+words[i+1]
+              yield (ans.lower(),1)
 
     
   ### input: self, in_key from mapper, in_value from mapper
   def reducer(self, key, values):
-      yield key, sum(values)
+      f.write('"' + str(key) + '"\t'+ str(sum(values)) + '\n')
 
-    #### Put your code here
 
 if __name__ == '__main__':
   BiGramFreqCount.run()
+
+f.close()
